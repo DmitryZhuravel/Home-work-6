bc5a1e3aa29135f806f423af9c2f0954e8dd27a7
«Dmitry» <d_m_i_t_r_I_I@mail.ru>
2022-03-30 02:36:45 +0500

    Точка пересечения координаты

---
+++ HW6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Задача 2. Напишите программу, которая найдёт точку 
+// пересечения двух прямых, заданных уравнениями y = k1 * x + b1, 
+// y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double Prompt(string message)
+{
+    Console.Write(message);
+    string strInput = Console.ReadLine();
+    double answer = double.Parse(strInput);
+    return answer;
+}
+double k1 = Prompt("Введите число k1 => ");
+double k2 = Prompt("Введите число k2 => ");
+double b1 = Prompt("Введите число b1 => ");
+double b2 = Prompt("Введите число b2 => ");
+double x; 
+double y;
+x=(b2-b1)/(k1-k2); 
+y=k1*(b2-b1)/(k1-k2)+b1;
+Console.WriteLine($"Координаты точки пересечения N({x}; {y})" );
